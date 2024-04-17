1aa7be411ae794ab7019ffd9f352c84f2b7437aa
Quentin Canu <qcanu17@gmail.com>
2024-04-17 16:39:14 +0200

    Improved verification algorithm implementation

---
+++ enumeration/theories/improved_graph_verif/low_algo.v
@@ -0,0 +1,77 @@
+From mathcomp Require Import all_ssreflect.
+From Coq Require Import PArray Uint63.
+From Bignums Require Import BigQ.
+From Polyhedra Require Import extra_misc.
+From PolyhedraHirsch Require Import extra_int extra_array rat_bigQ graph.
+(* From PolyhedraHirschVerif Require Import low_algo. *)
+
+
+Set   Implicit Arguments.
+Unset Strict Implicit.
+Unset Printing Implicit Defensive.
+Unset SsrOldRewriteGoalsOrder.
+
+Import Order.Theory.
+Local Notation int63 := Uint63.int.
+
+Module ImprovedVerif.
+
+Definition polyType := (array (array bigQ) * array bigQ)%type.
+
+(* 
+  x is the vertex to be considered, while X is the matrix A^-1.
+  the corresponding basis is given by I.
+*)
+
+Definition cmp_sat (Po : polyType) 
+  (I : array int63)
+  (x : array bigQ) (X : array (array bigQ)) 
+  (l : int63):=
+  let Al := Po.1.[l] in
+  ILex.lex_func2_rel_ (Uint63.succ (length Po.1))
+  BigQ.compare 0%uint63 true
+  (fun k i=> 
+    if (i =? 0)%uint63 then (BigQUtils.bigQ_dot Al x,k) else
+    if (Uint63.pred i =? I.[k])%uint63 then 
+      (BigQUtils.bigQ_dot Al X.[k],Uint63.succ k)
+    else (0%bigQ,k)
+  )
+  (fun k i=> 
+    if (i =? 0)%uint63 then (Po.2.[l],true) else
+    if (Uint63.pred i =? l)%uint63 then ((-1)%bigQ,true) else
+    (0%bigQ,true)
+  ).
+  
+Definition cmp_vect (Po : polyType) (I : array int63) (x : array bigQ) (X : array (array bigQ)) :=
+  let cmp := PArrayUtils.mk_fun 
+    (fun l=> cmp_sat Po I x X l)
+    (length Po.1) Eq
+  in 
+  (IFold.ifold (fun i '(acc,k)=>
+  match cmp.[i] with
+  |Lt => (false,k)
+  |Gt => (true,k)
+  |Eq => ((i =? I.[k])%uint63, Uint63.succ k)
+  end) (length cmp) (true,0%uint63)).1.
+
+Section Body.
+Context (Po : polyType) (G : graph).
+Context (lbl_bas : array (array int63)).
+Context (lbl_pt : array (array bigQ)).
+Context (lbl_inv : array (array (array bigQ))).
+
+(* We test validity of each vertex label*)
+
+Definition sat_test (i : int63) :=
+  let I := lbl_bas.[i] in
+  let x := lbl_pt.[i] in
+  let X := lbl_inv.[i] in
+  cmp_vect Po I x X.
+
+Definition n := length Po.1.[0%uint63].
+Definition card_bas_test (i : int63) := (length (lbl_bas.[i]) =? n)%uint63.
+Definition vertex_test (i : int63) := (card_bas_test i) && (sat_test i).
+  
+Definition vertex_consistent := IFold.iall (fun i=> vertex_test i) (length G).
+End Body.
+End ImprovedVerif.
