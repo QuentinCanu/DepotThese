4a524502b5086aa722c2d247f3b8ca60a1932c96
Quentin Canu <qcanu17@gmail.com>
2024-06-27 14:43:13 +0200

    -

--- enumeration/theories/improved_graph_verif/low_algo.v
+++ enumeration/theories/improved_graph_verif/low_algo.v
@@ -16,19 +16,20 @@ Local Notation int63 := Uint63.int.
 
 Module ImprovedVerif.
 
-Definition polyType := (array (array bigQ) * array bigQ)%type.
+Definition vector := array bigQ.
+Definition matrix := array vector.
 
 (* 
   x is the vertex to be considered, while X is the matrix A^-1.
   the corresponding basis is given by I.
 *)
 
-Definition cmp_sat (Po : polyType) 
+Definition cmp_sat (A : matrix) (b : vector) 
   (I : array int63)
   (x : array bigQ) (X : array (array bigQ)) 
   (l : int63):=
-  let Al := Po.1.[l] in
-  ILex.lex_func2_rel_ (Uint63.succ (length Po.1))
+  let Al := A.[l] in
+  ILex.lex_func2_rel_ (Uint63.succ (length A))
   BigQ.compare 0%uint63 true
   (fun k i=> 
     if (i =? 0)%uint63 then (BigQUtils.bigQ_dot Al x,k) else
@@ -37,15 +38,15 @@ Definition cmp_sat (Po : polyType)
     else (0%bigQ,k)
   )
   (fun k i=> 
-    if (i =? 0)%uint63 then (Po.2.[l],true) else
+    if (i =? 0)%uint63 then (b.[l],true) else
     if (Uint63.pred i =? l)%uint63 then ((-1)%bigQ,true) else
     (0%bigQ,true)
   ).
   
-Definition cmp_vect (Po : polyType) (I : array int63) (x : array bigQ) (X : array (array bigQ)) :=
+Definition cmp_vect (A : matrix) (b : vector) (I : array int63) (x : array bigQ) (X : array (array bigQ)) :=
   let cmp := PArrayUtils.mk_fun 
-    (fun l=> cmp_sat Po I x X l)
-    (length Po.1) Eq
+    (fun l=> cmp_sat A b I x X l)
+    (length A) Eq
   in 
   (IFold.ifold (fun i '(acc,k)=>
   match cmp.[i] with
@@ -55,7 +56,7 @@ Definition cmp_vect (Po : polyType) (I : array int63) (x : array bigQ) (X : arra
   end) (length cmp) (true,0%uint63)).1.
 
 Section Body.
-Context (Po : polyType) (G : graph).
+Context (A : matrix) (b : vector) (G : graph).
 Context (lbl_bas : array (array int63)).
 Context (lbl_pt : array (array bigQ)).
 Context (lbl_inv : array (array (array bigQ))).
@@ -66,9 +67,9 @@ Definition sat_test (i : int63) :=
   let I := lbl_bas.[i] in
   let x := lbl_pt.[i] in
   let X := lbl_inv.[i] in
-  cmp_vect Po I x X.
+  cmp_vect A b I x X.
 
-Definition n := length Po.1.[0%uint63].
+Definition n := length A.[0%uint63].
 Definition card_bas_test (i : int63) := (length (lbl_bas.[i]) =? n)%uint63.
 Definition vertex_test (i : int63) := (card_bas_test i) && (sat_test i).
   
