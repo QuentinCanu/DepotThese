e9c7fc2be9e053c6c61a35a9613ab64afde6c0f3
Quentin Canu <qcanu17@gmail.com>
2025-04-03 16:15:37 +0200

    -

--- enumeration/benchmarks/benchmark.py
+++ enumeration/benchmarks/benchmark.py
@@ -115,7 +115,7 @@ def gen_lrs(polytope, param):
       genlrs.generate_lrs(polytope, param)
 
 # --------------------------------------------------------------------
-def compute_lrs(name,_):
+def compute_lrs(name,_,_):
   inefile = os.path.join(DATA_DIR, name, "lrs", name+".ine")
   extfile = os.path.join(DATA_DIR, name, "lrs", name+".ext")
   time, memory = format_time_output(command_call(f"time lrs {inefile} {extfile}",prefix=TIME_MEM_PREFIX))
